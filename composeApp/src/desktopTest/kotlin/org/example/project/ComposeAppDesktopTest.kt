227eb2749736a18ddfa8729eb9503d707b84fde4
mailebrilhante <155116907+mailebrilhante@users.noreply.github.com>
2025-07-09 17:57:55 -0600

    Created unit tests to test current functionality of the Observer interface and Shipment class

--- composeApp/src/desktopTest/kotlin/org/example/project/ComposeAppDesktopTest.kt
+++ composeApp/src/desktopTest/kotlin/org/example/project/ComposeAppDesktopTest.kt
@@ -2,11 +2,68 @@ package org.example.project
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class ComposeAppDesktopTest {
+class ShipmentTest {
+
+    private class TestObserver : Observer {
+        var updateCalled = false
+        var updatedShipment: Shipment? = null
+
+        override fun update(shipment: Shipment) {
+            updateCalled = true
+            updatedShipment = shipment
+        }
+    }
 
     @Test
-    fun example() {
-        assertEquals(3, 1 + 2)
+    fun testRegisterObserver() {
+        val shipment = Shipment("123")
+        val observer = TestObserver()
+        shipment.registerObserver(observer)
+        shipment.addNote("Test note")
+        assertTrue(observer.updateCalled)
+        assertEquals(shipment, observer.updatedShipment)
+    }
+
+    @Test
+    fun testRemoveObserver() {
+        val shipment = Shipment("456")
+        val observer = TestObserver()
+        shipment.registerObserver(observer)
+        shipment.removeObserver(observer)
+        shipment.addNote("Another test note")
+        assertTrue(!observer.updateCalled)
+    }
+
+    @Test
+    fun testNotifyObserversOnAddNote() {
+        val shipment = Shipment("789")
+        val observer = TestObserver()
+        shipment.registerObserver(observer)
+        shipment.addNote("A note to trigger notification")
+        assertTrue(observer.updateCalled)
+        assertEquals(shipment, observer.updatedShipment)
+    }
+
+    @Test
+    fun testNotifyObserversOnAddUpdate() {
+        val shipment = Shipment("101")
+        val observer = TestObserver()
+        shipment.registerObserver(observer)
+        shipment.addUpdate("An update to trigger notification")
+        assertTrue(observer.updateCalled)
+        assertEquals(shipment, observer.updatedShipment)
+    }
+
+    @Test
+    fun testNotifyObserversOnApplyUpdate() {
+        val shipment = Shipment("112")
+        val observer = TestObserver()
+        shipment.registerObserver(observer)
+        shipment.applyUpdate({ s, _, _ -> s.status = "shipped" }, 12345L, null)
+        assertTrue(observer.updateCalled)
+        assertEquals(shipment, observer.updatedShipment)
+        assertEquals("shipped", shipment.status)
     }
 }
